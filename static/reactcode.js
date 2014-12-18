f0f5b7e12c5d481f944f9a72a09924e9206883df
Jonathan Warrick <jonathan.warrick@gmail.com>
2014-12-18 16:42:17 -0500

    Getting closer to React-wrapped D3.

---
+++ static/reactcode.js
@@ -0,0 +1,55 @@
+// React Code
+
+var Chart = React.createClass({
+	propTypes: {
+		data: React.PropTypes.array,
+		width: React.PropTypes.number,
+		height: React.PropTypes.number,
+		margin: React.PropTypes.object
+	},
+	componentDidMount: function() {
+		console.log(this.state);
+		var el = this.getDOMNode();
+		d3Chart.create(el, this.props, this.state)
+	},
+	componentDidUpdate: function() {
+		var el = this.getDOMNode();
+		d3Chart.update(el, this.props, this.state);
+	},
+	render: function() {
+    return (
+      <div className="Chart"></div>
+    );
+  }
+});
+
+var App = React.createClass({
+	getInitialState: function() {
+		return {
+			data: sampleData,
+			domain: {
+				x: this.data.map(function(d, i) {
+					return i;
+				}),
+				y: [Math.min(0, Math.min.apply(null, this.data)), Math.max.apply(null, this.data)]
+			}
+		};
+	},
+	render: function() {
+		return (
+			<div>
+				<Chart data={this.state.data}
+							 domain={this.state.domain}
+							 width={960}
+							 height={500}
+							 margin={
+							 	top: 20,
+							 	right: 30,
+							 	left: 30,
+							 	bottom: 40
+							 } 
+				/>
+			</div>
+		);
+	}
+});
